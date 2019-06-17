1b5f9d674149a7fa381b0079579cca95ee5d1f2d
Young <eyoung201@iastate.edu>
2019-06-17 15:13:37 -0500

    Sprint 3: Didn't have much time. Got a small bit done though.

--- app/src/main/java/com/example/ethanman04/memory/ThemeActivity.java
+++ app/src/main/java/com/example/ethanman04/memory/ThemeActivity.java
@@ -3,6 +3,8 @@ package com.example.ethanman04.memory;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ethanman04.allone.R;
 
@@ -12,6 +14,28 @@ public class ThemeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        setButtons();
+    }
+
+    private void setButtons(){
+        Button cancelButton = findViewById(R.id.memory_theme_cancel);
+        Button saveButton = findViewById(R.id.memory_theme_save);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThemeActivity.this, MemoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     /**
