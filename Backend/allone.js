7ab1bef8f67cba9da839c280de0c50f3c9243ed6
TrippyAcidCats <youngethan4@gmail.com>
2019-07-07 21:36:27 -0500

    Sprint 6: backend setup

--- Backend/allone.js
+++ Backend/allone.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql'); 
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "trippyacidcats",
+  password: "danny15GAY!"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
