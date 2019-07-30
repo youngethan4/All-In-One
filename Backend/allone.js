a46d8a17b9dfe66f34c903c87b73676a109c2e3a
TrippyAcidCats <youngethan4@gmail.com>
2019-07-30 14:10:09 -0500

    Sprint 6: security

--- Backend/allone.js
+++ Backend/allone.js
@@ -7,46 +7,36 @@ var  url = require('url');
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "trippyacidcats",
-  password: "danny15GAY!",
-  database: "allone"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+var con = require('./app/pass.js')(mysql);
 
 //Try this later once I get a feel of node js.
 //require('./app/routes/user.routes.js')(app, con);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.post("/api/register", (req, res) => {
-  console.log("\nNew user registering...");
-  var rq = url.parse(req.url, true);
-  var jsonData = JSON.parse(rq.querry.o);
+//app.post("/api/register", (req, res) => {
+//  console.log("\nNew user registering...");
+//  var rq = url.parse(req.url, true);
+//  var jsonData = JSON.parse(rq.querry.o);
 
-  var username = "" + jsonData.username;
-  var password = "" + jsonData.password;
-  var email = "" + jsonData.email;
-  var fname = "" + jsonData.fname;
-  var lname = "" + jsonData.lname;
+//  var username = "" + jsonData.username;
+//  var password = "" + jsonData.password;
+//  var email = "" + jsonData.email;
+//  var fname = "" + jsonData.fname;
+//  var lname = "" + jsonData.lname;
 
-  var  sql = "INSERT INTO users (username, password, email, firstname, lastname) VALUES ('";
-  sql += username + "', '" + password + "', '" + email + "', '" + firstname + "', '" + lastname + "')";
+//  var  sql = "INSERT INTO users (username, password, email, firstname, lastname) VALUES ('";
+//  sql += username + "', '" + password + "', '" + email + "', '" + firstname + "', '" + lastname + "')";
 
-  try{
-    con.query(sql, function(err, result){
-      if(err) throw err;
-      console.log("registered!");
-      var jsonReturn = {message:"User registered."};
-      res.json(jsonReturn);
-    });
-  }
-  catch (err){
-    console.log("Registering new user error.");
-  }
-}
+//  try{
+//    con.query(sql, function(err, result){
+//      if(err) throw err;
+//      console.log("registered!");
+//      var jsonReturn = {message:"User registered."};
+//      res.json(jsonReturn);
+ //   });
+//  }
+//  catch (err){
+//    console.log("Registering new user error.");
+//  }
+//});
