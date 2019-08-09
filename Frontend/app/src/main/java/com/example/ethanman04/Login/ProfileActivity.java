5972ecc70c711669658c9e6c67404ea485f679a5
Young <eyoung201@iastate.edu>
2019-08-09 15:45:48 -0500

    Sprint 7: Choose the avatars and started user profiles.

---
+++ Frontend/app/src/main/java/com/example/ethanman04/Login/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.ethanman04.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ethanman04.allone.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
